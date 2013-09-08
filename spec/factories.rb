370df92e3b47f61d2854005d3466a9c68068dbcc
ryohei <your.email@example.com>
2013-09-08 22:38:54 +0900

    Finish user edit, update, index, and destroy actions

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,8 +1,12 @@
 FactoryGirl.define do
   factory :user do
-    name     "Ryohei Takada"
-    email    "japanet-tk@docomo.ne.jp"
+    sequence(:name) { |n| "Person #{n}" }
+    sequence(:email) { |n| "person_#{n}@example.com"}
     password "foobar"
     password_confirmation "foobar"
+
+    factory :admin do #FactoryGirl.create(:admin)を実行すると:userも実行されるみたい。
+      admin true
+    end
   end
 end
